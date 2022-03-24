49ea20641e0acac85c0044608abb01ed88e0fcfe
dynamo58 <sailorfirex@seznam.cz>
2022-03-24 06:53:59 +0100

    xd

--- controller.ts
+++ controller.ts
@@ -1,9 +1,11 @@
 import { Context } from 'https://deno.land/x/abc@v1.3.3/mod.ts';
+import { acceptWebSocket } from 'https://deno.land/x/abc@v1.0.3/vendor/https/deno.land/std/ws/mod.ts';
+import { HandlerFunc } from 'https://deno.land/x/abc@v1.0.3/types.ts';
 import { v4 } from "https://deno.land/std/uuid/mod.ts"
 
 export const getEmote = async (c: Context) => {
 	const { emoteName } = c.params;
-	return c.file(`public/${emoteName}.webp`);
+	return c.file(`public/img/${emoteName}.webp`);
 }
 
 export const getHome = async (c: Context) => {
@@ -23,5 +25,18 @@ export const game = async (c: Context) => {
 }
 
 // WebSockets
+// export const socket: HandlerFunc = async (c: Context) => {
+// 	const { emoteName } = c.params;
+// 	const { conn, headers, r: bufReader, w: bufWriter } = c.request;
+//   	const ws = await acceptWebSocket({
+// 		conn,
+// 		headers,
+// 		bufReader,
+// 		bufWriter,
+// 	});
 
-export const socketHandler = async (c: Context, roomId: uuid)
+// 	for await (const e of ws) {
+// 		console.log(e);
+// 		await ws.send("Hello, Client!");
+// 	}
+// }
--- main.ts
+++ main.ts
@@ -1,34 +1,24 @@
 import { Application } from 'https://deno.land/x/abc@v1.3.3/mod.ts';
-import { HandlerFunc } from 'https://deno.land/x/abc@v1.0.3/types.ts';
-import { acceptWebSocket } from 'https://deno.land/x/abc@v1.0.3/vendor/https/deno.land/std/ws/mod.ts';
-
 import { getEmote, getHome, joinRoom, createRoom, game } from './controller.ts';
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 
 const app = new Application();
 
-const hello: HandlerFunc = async (c) => {
-  const { conn, headers, r: bufReader, w: bufWriter } = c.request;
-  const ws = await acceptWebSocket({
-    conn,
-    headers,
-    bufReader,
-    bufWriter,
-  });
-
-  for await (const e of ws) {
-    console.log(e);
-    await ws.send("Hello, Client!");
-  }
-};
-
-app.get("/ws", hello).file("/", "./index.html").start({ port: 8080 });
-
 app.static('/', './public');
-console.log(`App listening on http://localhost:8080/`);
+
+
+const wss = new WebSocketServer(3001);
+wss.on("connection", function (ws: WebSocketClient) {
+    ws.on("message", function (message: string) {
+        console.log(message);
+        ws.send(message);
+    });
+});
 
 app
   .get('/', getHome)
   .get('/home', getHome)
-  .get('/room/:roomId')
+  // .get('/room/:roomId', socket)
   .get('emotes/:emoteName', getEmote)
-  .start({ port: 8080 });
+  .start({ port: 3000 });
+console.log(`App listening on http://localhost:3001/`);
